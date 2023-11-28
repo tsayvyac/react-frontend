69f42a2952560ebaac43d0cb113e4aa24c08d033
Tsay, Vyacheslav <tsayvyac@fel.cvut.cz>
2023-11-28 15:44:05 +0100

    Added new button "Compare" and limit of checked checkboxes.

--- src/parts/Services.jsx
+++ src/parts/Services.jsx
@@ -3,8 +3,9 @@ import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
 import {ThemeProvider} from "@mui/material/styles";
 import Container from "@mui/material/Container";
+import CompareIcon from '@mui/icons-material/Compare';
 import {
-    Card,
+    Card, Fab,
     Paper,
     Table,
     TableBody,
@@ -81,6 +82,22 @@ const ServicesTable = () => {
     const [rowsPerPage, setRowsPerPage] = useState(10);
     const cellAlign = 'left';
     const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+    const [checked, setChecked] = useState(new Array(rows.length).fill(false));
+
+    const handleCheckbox = (position, e) => {
+        if (checked.filter((i) => i).length >= 3 && e.target.checked) return;
+        const updated = checked.map((item, index) => index === position ? !item : item);
+        setChecked(updated);
+    };
+
+    const handleDisabled = (index) => {
+        return checked.filter((p) => p === true).length >= 3 &&
+            !checked[index];
+    };
+
+    const handleFab = () => {
+        return checked.filter((i) => i).length < 2;
+    }
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -100,49 +117,69 @@ const ServicesTable = () => {
     );
 
     return (
-        <Card>
-            <TableContainer component={Paper}>
-                <Table>
-                    <ServicesTableHead/>
-                    <TableBody>
-                        {visibleRows.map((row) => {
-                            return (
+        <React.Fragment>
+            <Card>
+                <TableContainer component={Paper}>
+                    <Table>
+                        <ServicesTableHead/>
+                        <TableBody>
+                            {visibleRows.map((row, index) => {
+                                return (
+                                    <TableRow
+                                        hover
+                                        sx={{cursor: 'pointer'}}
+                                    >
+                                        <TableCell align={cellAlign}>{row.name}</TableCell>
+                                        <TableCell align={cellAlign}>{row.phone}</TableCell>
+                                        <TableCell align={cellAlign}>{row.location}</TableCell>
+                                        <TableCell align={cellAlign}>{row.rr}</TableCell>
+                                        <TableCell align='right' padding="checkbox">
+                                            <Checkbox
+                                                disabled={handleDisabled(index)}
+                                                checked={checked[index]}
+                                                onChange={(e) => handleCheckbox(index, e)}
+                                            />
+                                        </TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                            {emptyRows > 0 && (
                                 <TableRow
-                                    hover
-                                    sx={{cursor: 'pointer'}}
+                                    style={{
+                                        height: 53 * emptyRows,
+                                    }}
                                 >
-                                    <TableCell align={cellAlign}>{row.name}</TableCell>
-                                    <TableCell align={cellAlign}>{row.phone}</TableCell>
-                                    <TableCell align={cellAlign}>{row.location}</TableCell>
-                                    <TableCell align={cellAlign}>{row.rr}</TableCell>
-                                    <TableCell align='right' padding="checkbox">
-                                        <Checkbox/>
-                                    </TableCell>
+                                    <TableCell colSpan={6}/>
                                 </TableRow>
-                            );
-                        })}
-                        {emptyRows > 0 && (
-                            <TableRow
-                                style={{
-                                    height: 53 * emptyRows,
-                                }}
-                            >
-                                <TableCell colSpan={6}/>
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-                <TablePagination
-                    count={rows.length}
-                    component="div"
-                    page={page}
-                    rowsPerPage={rowsPerPage}
-                    rowsPerPageOptions={[10, 20, 50]}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                />
-            </TableContainer>
-        </Card>
+                            )}
+                        </TableBody>
+                    </Table>
+                    <TablePagination
+                        count={rows.length}
+                        component="div"
+                        page={page}
+                        rowsPerPage={rowsPerPage}
+                        rowsPerPageOptions={[10, 20, 50]}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                </TableContainer>
+            </Card>
+            <Fab disabled={handleFab()}
+                 variant="extended"
+                 color="primary"
+                 style={{
+                     position: 'fixed',
+                     top: 'auto',
+                     left: 'auto',
+                     margin: 0,
+                     right: 40,
+                     bottom: 20}}
+            >
+                <CompareIcon/>
+                Compare
+            </Fab>
+        </React.Fragment>
     );
 }
 
@@ -151,11 +188,11 @@ const ServicesTableHead = () => {
     return (
         <TableHead>
             <TableRow>
-                <TableCell align="left">Public service name</TableCell>
-                <TableCell align="left">Phone</TableCell>
-                <TableCell align="left">Location</TableCell>
-                <TableCell align="left">Reserved/Resolved</TableCell>
-                <TableCell align="left">Compare</TableCell>
+                <TableCell align="left"><Typography fontWeight="bold" >Public service name</Typography></TableCell>
+                <TableCell align="left"><Typography fontWeight="bold" >Phone</Typography></TableCell>
+                <TableCell align="left"><Typography fontWeight="bold" >Location</Typography></TableCell>
+                <TableCell align="left"><Typography fontWeight="bold" >Reserved/Resolved</Typography></TableCell>
+                <TableCell align="left"><Typography fontWeight="bold" >Compare</Typography></TableCell>
             </TableRow>
         </TableHead>
     );
