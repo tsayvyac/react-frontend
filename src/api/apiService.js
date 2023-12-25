f1a28e1ae852523ad00b4acd87ef7adcf26ee24c
nurkharl <arlan.nurkhozhin@gmail.com>
2023-12-25 15:43:54 +0100

    fix null bearer token

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -16,12 +16,17 @@ function setToken(token) {
 }
 
 const instance = axios.create({
-   baseURL: 'https://bettercity.mikita.dev/',
+   baseURL: 'https://bettercity.mikita.dev/api/v1',
    headers: {
       'Content-type': 'application/json',
       Authorization: `Bearer ${accessToken}`
    }
 });
+
+instance.interceptors.request.use(config => {
+   config.headers.Authorization = `Bearer ${accessToken}`;
+   return config;
+})
 
 function getServices() {
    return instance.get('/services');
